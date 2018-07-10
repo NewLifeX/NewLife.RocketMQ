ca7965b0bc3155a8f1f04b8542d58371d84a074d
大石头 <nnhy@vip.qq.com>
2018-07-10 09:37:18 +0800

    http://git.newlifex.com/NewLife/NewLife.RocketMQ

---
+++ Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using NewLife.Log;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            XTrace.UseConsole();
+        }
+    }
+}
