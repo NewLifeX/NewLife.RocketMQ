36824891a1ecf4be0295a07a339a51441c97e45c
智能大石头 <nnhy@vip.qq.com>
2025-06-21 10:14:08 +0800

    [NewLife.RocketMQ] master 自动提交

--- NewLife.RocketMQ/Protocol/ConsumerData.cs
+++ NewLife.RocketMQ/Protocol/ConsumerData.cs
@@ -1,26 +1,25 @@
-﻿namespace NewLife.RocketMQ.Protocol
+﻿namespace NewLife.RocketMQ.Protocol;
+
+/// <summary>消费者数据</summary>
+public class ConsumerData
 {
-    /// <summary>消费者数据</summary>
-    public class ConsumerData
-    {
-        #region 属性
-        /// <summary>从哪里开始消费</summary>
-        public String ConsumeFromWhere { get; set; } = "CONSUME_FROM_LAST_OFFSET";
+    #region 属性
+    /// <summary>从哪里开始消费</summary>
+    public String ConsumeFromWhere { get; set; } = "CONSUME_FROM_LAST_OFFSET";
 
-        /// <summary>消费类型</summary>
-        public String ConsumeType { get; set; } = "CONSUME_ACTIVELY";
+    /// <summary>消费类型</summary>
+    public String ConsumeType { get; set; } = "CONSUME_ACTIVELY";
 
-        /// <summary>组名</summary>
-        public String GroupName { get; set; }
+    /// <summary>组名</summary>
+    public String GroupName { get; set; }
 
-        /// <summary>消息模型。广播/集群</summary>
-        public String MessageModel { get; set; } = "CLUSTERING";
+    /// <summary>消息模型。广播/集群</summary>
+    public String MessageModel { get; set; } = "CLUSTERING";
 
-        /// <summary>订阅数据集</summary>
-        public SubscriptionData[] SubscriptionDataSet { get; set; }
+    /// <summary>订阅数据集</summary>
+    public SubscriptionData[] SubscriptionDataSet { get; set; }
 
-        /// <summary>单元模式</summary>
-        public Boolean UnitMode { get; set; }
-        #endregion
-    }
+    /// <summary>单元模式</summary>
+    public Boolean UnitMode { get; set; }
+    #endregion
 }
